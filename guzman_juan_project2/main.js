3ab04ad492b74fbf8e05fb15d7cf6ab6cad36df7
Juan Guzman <mrguzman83@fullsail.edu>
2012-11-29 18:11:10 -0800

    Began adding variables and functions to main.js

---
+++ guzman_juan_project2/main.js
@@ -0,0 +1,23 @@
+// Project 2
+// Visual Frameworks 
+// By Juan J Guzman
+
+window.addEventListener("DOMContenLoaded", function(){
+
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement	
+	}	
+	
+	
+	var displayAll = $('displayAll');
+	displayAll.addEventListener()
+	
+	
+	
+	
+	
+	
+	
+	
+});
