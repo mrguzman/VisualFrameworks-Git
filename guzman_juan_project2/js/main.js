8b3b03d8837b1df92e2555a92005cb3d124f9bc6
Juan Guzman <mrguzman83@fullsail.edu>
2012-11-29 19:09:17 -0800

    JS is not being applied to my HTML (?) :/

---
+++ guzman_juan_project2/js/main.js
@@ -0,0 +1,46 @@
+// Project 2
+// Visual Frameworks 
+// By Juan J Guzman
+
+window.addEventListener("DOMContenLoaded", function(){
+
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement	
+	}	
+	
+	
+	function makeSourceOpt(){
+		var formTag = document.getElementsByTagName("form"),
+			selectLi = $('leadSource'),
+			makeSelect = document.createElement('select');
+			makeSelect.setAttribute("id", "leadSources");
+		for (var i=0, j=sources.length; i<j; i++){
+			var makeOpt = document.createElement('option');
+			var optText = sources[i];
+			makeOpt.setAttribute("value", optText);
+			makeOpt.innerHTML = optText;
+			makeSelect.appendChild(makeOpt);
+		}
+		selectLi.appendChild(makeSelect);			
+	}
+	
+	
+	var sources = ["--Select Lead Source--", "Walk-In", "Referral", "Response to Ad", "Cold Call"];
+	makeSourceOpt();
+	
+	var displayAll = $('displayAll');
+	displayAll.addEventListener("click", getData);
+	var clearData = $('clearData');
+	clearData.addEventListener("click", clearData);
+	var scheduleButton = $('submit');
+	scheduleButton.addEventListener("click", scheduleLead);
+	
+	
+	
+	
+	
+	
+	
+	
+});
