d0dbb109fdc3e37587287dad9d51c0f1b79d2b02
Juan Guzman <mrguzman83@fullsail.edu>
2012-12-06 19:19:39 -0800

    Adjusted some CSS, Local Storage still not working.

--- guzman_juan_project2/js/main.js
+++ guzman_juan_project2/js/main.js
@@ -7,17 +7,16 @@ window.addEventListener("DOMContentLoaded", function(){
 	
 	function $(x){
 		var theElement = document.getElementById(x);
-		return theElement	
+		return theElement;	
 	}	
 	
-	//Populate field options for lead source
-	
+	//Populate field options for lead sources
 	function makeSourceOpt(){
 		var formTag = document.getElementsByTagName("form"),
 			selectLi = $('leadSource'),
 			makeSelect = document.createElement('select');
 			makeSelect.setAttribute("id", "leadSources");
-		for (var i=0, j=sources.length; i<j; i++){
+		for (var i=0, j= sources.length; i<j; i++){
 			var makeOpt = document.createElement('option');
 			var optText = sources[i];
 			makeOpt.setAttribute("value", optText);
@@ -33,7 +32,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		var radios = document.forms(0).preference;
 		for(var i=0; i<radios.length; i++){
 			if(radios[i].checked){
-				timeValue = radios[i].value;	
+				var timeValue = radios[i].value;	
 			}
 		}
 	}
@@ -60,11 +59,11 @@ window.addEventListener("DOMContentLoaded", function(){
 		}
 	}
 	
-	// Save to Local Storage
+	// Save to Local Storage (Something is broken here)
 	
 	function saveLeads(){
-		var id 				= Math.floor(Math.random()*100000001);
-			getSelectedRadio();
+		var id 					= Math.floor(Math.random()*100000001);
+			 getSelectedRadio();
 		var item				= {};
 			item.fname			= ["First Name:", $('fname').value];
 			item.lname			= ["Last Name:", $('lname').value];
@@ -95,25 +94,97 @@ window.addEventListener("DOMContentLoaded", function(){
 		$('items').style.display = "block";
 		for (var i=0, len=localStorage.length; i<len; i++){
 			var makeLi = document.createElement('li');
+			var linksLi = document.createElement('li'); 
 			makeList.appendChild(makeLi);
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
 			var obj = JSON.parse(value);
-			var makeSublist = document.createElement('ul');
+			var makeSubList = document.createElement('ul');
 			makeLi.appendChild(makeSublist);
 			for (var n in obj){
 				var makeSubLi = document.createElement('li');
 				makeSublist.appendChild(makeSubLi);
 				var optSubText = obj[n][0]+" "+obj[n][1];
 				makeSubLi.innerHTML = optSubText;
+				makeSubList.appendChild(linksLi);
 			}
+			makeItemLinks(localStorage.key(i), linksLi); 
 		}
 	}
 	
+	//Make Item links function. Create edit/delete for each item in local storage.
+	
+	function makeItemLinks(key, linksLi){
+		var editLink = document.createElement('a');
+		editLink.href = "#";
+		editLink.key = key;
+		var editText = "Edit Callback";
+		editLink.addEventListener("click", editLead);
+		editLink.innerHTML = editText;
+		linksLi.appendChild(editLink);
+		
+		var breakTag = document.createElement("br");
+		linksLi.appendChild(breakTag);
+		
+		var deleteLink = document.createElement('a');
+		deleteLink.href = "#";
+		deleteLink.key = key;
+		var deleteText = "Delete Callback";
+		deleteLink.addEventListener("click", deleteLead);
+		deleteLink.innerHTML = deleteText;
+		deleteLink.appendChild(deleteLink);
+		
+	}
+	
+	function editLead(){
+		//Grab data from Local storage
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
+		
+		//unhide Form
+		toggleControls("off");
+		
+		//populate form with value 
+		$('fname').value = item.fname[1];
+		$('lname').value = item.lname[1];
+		$('contactNum').value = item.contactNum[1];
+		$('contactType').value = item.contactType[1];
+		$('date').value = item.date[1];
+		var radios = document.forms[0].preference;
+		for (var i=0 ; i<radios.length; i++){
+			if (radios[i].value == "Morning" && item.preference[1] == "Morning") {
+				radios[i].setAttribute("checked", "checked");
+			}else if(radios[i].value =="Afternoon" && item.preference[1] == "Afternoon"){
+				radios[i].setAttribute("checked", "checked");
+			}else if(radios[i].value == "Evening" && item.preference[1] == "Evening"){
+				radios[i].setAttribute("checked", "checked");
+			}else if(radios[i].value == "Anytime" && item.preference[1] == "Anytime"){
+				radios[i].setAttribute("checked", "checked");
+			}
+		}
+		$('interestLevel').value = item.interestLevel[1];
+		$('leadSources').value = item.leadSources[1];
+		$('comments').value = item.comments[1];
+	}; 
+	
+		//Remove Listener from "Schedule Callback" button
+		
+		scheduleButton.removeEventListener("click", saveLeads);
+		
+		//Change "Schedule Callback" to "Edit Callback"
+		
+		$('submit').value = "Edit Callback";
+		var editScheduleButton = $('submit');
+		editScheduleButton.addEventListener("click", validate);
+		editScheduleButton.key = this.key;
+		 
+
+
+
 	
 	function clearData(){
 		if(localStorage.length === 0){
-			alert("No Leads Are Currently Scheduled")
+			alert("No Leads Are Currently Scheduled");
 		}else {
 			localStorage.clear();
 				alert("All Leads Are Deleted");
@@ -125,8 +196,8 @@ window.addEventListener("DOMContentLoaded", function(){
 	// Dropdown variable defaults
  
 	var sources = ["--Select Lead Source--", "Walk-In", "Referral", "Response to Ad", "Cold Call"],
-		timevalue
-;
+		timevalue;
+		
 	makeSourceOpt();
 	
 	//Navigation controls and links
