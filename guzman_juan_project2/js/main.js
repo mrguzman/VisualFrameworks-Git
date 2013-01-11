81fabfd686cb10523fb0f102e96ad85d7439db3a
Juan Guzman <mrguzman83@fullsail.edu>
2013-01-10 22:18:41 -0800

    Building JS file

--- guzman_juan_project2/js/main.js
+++ guzman_juan_project2/js/main.js
@@ -6,8 +6,23 @@
 
 window.addEventListener("DOMContentLoaded", function(){
 	
+	//Function to help return HTML elements by their ID
+	
+	function $(x){
+		var htmlID = document.getElementById(x);
+		return htmlID;
+	}
 	
 	
+	
+	// Controls to display and clear all leads
+	
+	var displayAll = $('displayAll');
+	displayAll.addEventListener("click", getData);
+	var clearAll = $('clearLeads');
+	clearAll.addEventListener("click", clearData);
+	var saveButton = $('submit');
+	saveButton.addEventListener("click", saveLead); 
 	
 	
 	
