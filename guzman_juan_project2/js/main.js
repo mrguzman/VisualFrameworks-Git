d64c0a22462b8232d57547f565dfe2f0875b1d86
Juan Guzman <mrguzman83@fullsail.edu>
2013-01-11 18:00:54 -0800

    Created local storage function…testing.

--- guzman_juan_project2/js/main.js
+++ guzman_juan_project2/js/main.js
@@ -17,12 +17,35 @@ window.addEventListener("DOMContentLoaded", function(){
 	
 	// Controls to display and clear all leads
 	
-	var displayAll = $('displayAll');
+/*	var displayAll = $('displayAll');
 	displayAll.addEventListener("click", getData);
 	var clearAll = $('clearLeads');
 	clearAll.addEventListener("click", clearData);
-	var saveButton = $('submit');
-	saveButton.addEventListener("click", saveLead); 
+	var saveButton = $('submit');*/
+	saveButton.addEventListener("click", saveLead);
+	
+	
+	
+	//Save info to local storage
+	
+	
+	function saveLead(){
+		var id = math.floor(Math.random()*1000000001);
+		var item = {};
+			item.fname = ["First Name:", $('fname').value];
+			item.lname = ["Last Name:", $('lname').value];
+			item.contactNum = ["Contact Number:", $('contactNum').value];
+			item.contactType = ["Contact Type:", $('contactType').value];
+			item.date = ["Scheduled Date:", $('date').value];
+			item.timeOfDay = ["Preferred Time:", timeValue];
+			item.interestLevel = ["Interest Level", $('interestLevel').value];
+			item.leadSource = ["Lead Source:", $('leadSource').value];
+			item.comments = ["Comments:", $('comments').value];
+			localStorage.setItem(id, JSON.stringify(item));
+			
+			alert("Callback Scheduled");
+		
+	} 
 	
 	
 	
