15ca245b594e0ad6df88ad579f06a25e14e9c94e
Juan Guzman <mrguzman83@fullsail.edu>
2013-01-14 21:00:19 -0800

    Organized js code

--- guzman_juan_project2/js/main.js
+++ guzman_juan_project2/js/main.js
@@ -6,7 +6,7 @@
 
 window.addEventListener("DOMContentLoaded", function(){
 	
-	//Function to help return HTML elements by their ID
+//Function to help return HTML elements by their ID
 	
 	function $(x){
 		var htmlID = document.getElementById(x);
@@ -15,28 +15,26 @@ window.addEventListener("DOMContentLoaded", function(){
 	
 	
 	
-	// Controls to display and clear all leads
+// Controls to display and clear all leads
 	
 /*	var displayAll = $('displayAll');
 	displayAll.addEventListener("click", getData);
 	var clearAll = $('clearLeads');
 	clearAll.addEventListener("click", clearData);  */
-	var saveButton = $('submit');
-	saveButton.addEventListener("click", saveLead);
+
 	
 	
-	
-	//Dynamic dropdown for "Select Lead Source"
+//Dynamic dropdown for "Select Lead Source"
 	
 	function createDropdown(){
-		var grabTag = document.getElementsByTagName("form"),
-		grabLi = $('leadSource'),
-		createSelect = document.createElement('select');
-		createSelect.setAttribute("id", "leadSources");
-		for (var i=0, j = sources.length; i<j; i++){
-			var createOpt = document.createElement('option');
-			var optText = sources[i];
-			createOpt.setAttribute("value", optText);
+		var grabTag = document.getElementsByTagName("form"),	//Targets <form></form> tag in additem.html
+		grabLi = $('leadSource'),								//Targets and grabs <li><label for="leadSource" id="leadSource">Select Lead Source:</label></li>
+		createSelect = document.createElement('select');		//Creates <select></select> tags within <li></li>
+		createSelect.setAttribute("id", "leadtypes");				//Adds id="sources" to <select></select> (<select id="leadTypes"></select>)
+		for (var i=0, j = leadType.length; i<j; i++){			//Loops through "var = sources" and retrieves items in array
+			var createOpt = document.createElement('option');	//Creates <option></option> tag for each item in array
+			var optText = leadType[i];														
+			createOpt.setAttribute("value", optText);			//Adds value="" for each <option></option> tag (<option value="walkIn">Walk-In</option>)
 			createOpt.innerHTML = optText;
 			createSelect.appendChild(createOpt)
 		}
@@ -44,14 +42,33 @@ window.addEventListener("DOMContentLoaded", function(){
 	}
 	
 	
-	//Array to contain dropdown options.
 	
-	var sources = ["--Select Lead Source--", "Walk-In", "Referral", "Response to Ad", "Cold Call"],
+//Array to contain dropdown options.
+	
+	var leadType = ["--Select Lead Source--", "Walk-In", "Referral", "Response to Ad", "Cold Call"],
 					timeValue;
+					
+					createDropdown();
+					
+
+//Save info to local storage
 	
 	
-	//Save info to local storage
+		var saveButton = $('submit');
+	saveButton.addEventListener("click", saveLead);
+
+	// Selected Radio button function to get value and save to local storage
 	
+	function getRadio(){
+		var radios = document.forms[0].timeOfDay;
+		for(var i=0; i<radios.length; i++){
+			if(radios[i].checked){
+				timeValue = radios[i].value;
+			}
+		}
+	}
+	
+	// Local Storage save function
 	
 	function saveLead(){
 		var id = Math.floor(Math.random()*1000000001);
@@ -73,18 +90,7 @@ window.addEventListener("DOMContentLoaded", function(){
 	} 
 	
 	
-	// Selected Radio button
-	
-	
-	function getRadio(){
-		var radios = document.forms[0].timeOfDay;
-		for(var i=0; i<radios.length; i++){
-			if(radios[i].checked){
-				timeValue = radios[i].value;
-			}
-		}
-	}
-	
+
 	
 	
 	
