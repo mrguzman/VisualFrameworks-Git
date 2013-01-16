2361eee9b9b3f1b6082c492bebc278baa517195d
Juan Guzman <mrguzman83@fullsail.edu>
2013-01-15 19:25:06 -0800

    Trying to add a sort function….

--- guzman_juan_project2/js/main.js
+++ guzman_juan_project2/js/main.js
@@ -164,6 +164,43 @@ window.addEventListener("DOMContentLoaded", function(){
 				return false;
 			}
 		}
+		
+		
+	//Sort UL list....at least that is what I was trying to do.
+	
+	
+	function sortUnorderedList(ul, sortDescending) {
+	  if(typeof ul == "string"){
+	    	ul = document.getElementById(ul);
+	    	}
+	 	  if(!ul) {
+		    alert("The UL object is null!");
+		    return;
+		  }
+		  
+	  // Get the list items and setup an array for sorting
+	  var lis = ul.getElementsByTagName("LI");
+	  var vals = [];
+	
+	  // Populate the array
+	  for(var i = 0, l = lis.length; i < l; i++)
+	    vals.push(lis[i].innerHTML);
+	
+	  // Sort it
+	  vals.sort();
+	
+	  // Descending
+	  if(sortDescending){
+	    	vals.reverse();
+	    	
+	    	}
+	
+	  // Change the list on the page
+	  for(var i = 0, l = lis.length; i < l; i++){
+	    lis[i].innerHTML = vals[i];
+	    }
+	}
+	
 	
 	
 	
