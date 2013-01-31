71687deb05c083980f0496f052c107259dbb69d7
Juan Guzman <mrguzman83@fullsail.edu>
2013-01-30 18:28:03 -0800

    Created json

---
+++ guzman_juan_project4/js/json.js
@@ -0,0 +1,35 @@
+var placeHolder = {
+	"lead1": {
+		"fname": ["First Name:", "Juan"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075557959"],
+		"contactType": ["Contact Type:", "Cell"],
+		"date": ["Schedule Call Date:", "2012-12-20"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "8"],
+		"leadSource": ["Lead Source:", "Walk-In"],
+		"comments": ["Callback Commens:", "Notes"]
+ 	},
+ 		"lead2": {
+		"fname": ["First Name:", "Nena"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075552008"],
+		"contactType": ["Contact Type:", "Cell"],
+		"date": ["Schedule Call Date:", "2012-12-22"],
+		"timeOfDay": ["Preferred Time:", "Afternoon"],
+		"interestLevel": ["Select Interest Level:", "10"],
+		"leadSource": ["Lead Source:", "Referral"],
+		"comments": ["Callback Commens:", "Spouse"]
+ 	},
+ 		"lead3": {
+		"fname": ["First Name:", "Evan"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075558959"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	}
+}
